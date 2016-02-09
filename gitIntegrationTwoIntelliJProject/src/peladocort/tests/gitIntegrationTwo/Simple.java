c1a8a5a56b1766c48de0719f0d36e66fd9dd3b90
HoraBla <hb347x@gmail.com>
2016-02-08 23:17:57 -0300

    A change in master branch

--- gitIntegrationTwoIntelliJProject/src/peladocort/tests/gitIntegrationTwo/Simple.java
+++ gitIntegrationTwoIntelliJProject/src/peladocort/tests/gitIntegrationTwo/Simple.java
@@ -7,5 +7,6 @@ public class Simple {
     public static void main(String[] args) {
         System.out.println("Simple!");
         System.out.println("An extra line");
+        //A change in master
     }
 }
